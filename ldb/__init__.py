3a8e2e9fd9452e88ea67752b7acf2b33c9487871
Jon Burdo <jon@jonburdo.com>
2021-10-04 16:17:00 -0400

    add pre-commit Fixes #19

--- ldb/__init__.py
+++ ldb/__init__.py
@@ -1,3 +1,6 @@
+"""
+LDB base module
+"""
 __all__ = ["__version__"]
 
 from .version import __version__
--- ldb/version.py
+++ ldb/version.py
@@ -1,7 +1,13 @@
+"""
+Version info management
+"""
 __all__ = ["__version__"]
 
 import os
 
-with open(os.path.join(os.path.dirname(__file__), "VERSION")) as version_file:
+with open(
+    os.path.join(os.path.dirname(__file__), "VERSION"),
+    encoding="utf-8",
+) as version_file:
     __version__ = version_file.read().strip()
 del os, version_file
