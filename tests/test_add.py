0a23cbf831310fc703baae409af01c256aaa05d3
Jon Burdo <jon@jonburdo.com>
2021-11-15 22:04:02 -0500

    simplify test for add command

--- tests/test_add.py
+++ tests/test_add.py
@@ -4,9 +4,6 @@ from typing import Iterable, List
 
 from ldb.main import main
 from ldb.path import WorkspacePath
-from ldb.stage import stage_workspace
-from ldb.utils import current_time
-from ldb.workspace import WorkspaceDataset
 
 
 def get_staged_object_file_paths(workspace_path: Path) -> List[Path]:
@@ -20,19 +17,8 @@ def num_empty_files(paths: Iterable[Path]) -> int:
     return num
 
 
-def test_add_storage_location(tmp_path, data_dir, ldb_instance):
-    workspace_path = tmp_path / "workspace"
+def test_add_storage_location(workspace_path, data_dir):
     dir_to_add = data_dir / "fashion-mnist/original"
-    stage_workspace(
-        workspace_path,
-        WorkspaceDataset(
-            dataset_name="my-new-dataset",
-            staged_time=current_time(),
-            parent="",
-            tags=[],
-        ),
-    )
-    os.chdir(workspace_path)
     ret = main(["add", f"{os.fspath(dir_to_add)}"])
     object_file_paths = get_staged_object_file_paths(workspace_path)
     assert ret == 0
