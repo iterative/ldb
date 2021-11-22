b2bc1e9ac5b1945972819af601353e8baddeadad
Jon Burdo <jon@jonburdo.com>
2021-11-22 17:31:04 -0500

    improve test type annotations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import os
 from pathlib import Path
+from typing import Generator, NoReturn
 
 import pytest
+from pytest import MonkeyPatch, TempPathFactory
 
 from ldb.config import get_global_base, set_default_instance
 from ldb.core import init
@@ -32,7 +34,7 @@ def pytest_configure(config):
 
 
 @pytest.fixture(scope="session")
-def monkeypatch_session():
+def monkeypatch_session() -> Generator[MonkeyPatch, None, None]:
     """Like monkeypatch, but for session scope."""
     mpatch = pytest.MonkeyPatch()
     yield mpatch
@@ -40,13 +42,16 @@ def monkeypatch_session():
 
 
 @pytest.fixture(scope="session", autouse=True)
-def clean_environment(monkeypatch_session, tmp_path_factory):
+def clean_environment(
+    monkeypatch_session: MonkeyPatch,
+    tmp_path_factory: TempPathFactory,
+) -> None:
     """Make sure we have a clean environment and won't write to userspace."""
     working_dir = tmp_path_factory.mktemp("default_working_dir")
     os.chdir(working_dir)
     monkeypatch_session.delenv(Env.LDB_DIR, raising=False)
 
-    def raise_exc():
+    def raise_exc() -> NoReturn:
         raise NotImplementedError(
             "To get a tmp path, use fixture global_base",
         )
@@ -58,8 +63,8 @@ def clean_environment(monkeypatch_session, tmp_path_factory):
 
 
 @pytest.fixture
-def global_base(monkeypatch, tmp_path):
-    def get_tmp_global_base_parent():
+def global_base(monkeypatch: MonkeyPatch, tmp_path: Path) -> Path:
+    def get_tmp_global_base_parent() -> Path:
         home = Path.home()
         return tmp_path / home.relative_to(home.anchor)
 
@@ -71,7 +76,7 @@ def global_base(monkeypatch, tmp_path):
 
 
 @pytest.fixture
-def ldb_instance(tmp_path: Path, global_base, data_dir) -> Path:
+def ldb_instance(tmp_path: Path, global_base: Path, data_dir: Path) -> Path:
     instance_dir = tmp_path / "ldb_instance"
     init(instance_dir)
     set_default_instance(instance_dir, overwrite_existing=True)
