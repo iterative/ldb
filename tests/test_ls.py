bfc9c2f775c811df5269a0950f84ba541af07c2e
Jon Burdo <jon@jonburdo.com>
2021-11-29 18:37:05 -0500

    fix path seps in tests

--- tests/test_ls.py
+++ tests/test_ls.py
@@ -19,55 +19,55 @@ ORIGINAL_DIR = DATA_DIR / "fashion-mnist/original"
 UPDATES_DIR_LISTINGS = [
     DatasetListing(
         data_object_hash="31ed21a2633c6802e756dd06220b0b82",
-        data_object_path=osp.join(UPDATES_DIR, "no_inference/00028.png"),
+        data_object_path=osp.join(UPDATES_DIR, "no_inference", "00028.png"),
         annotation_hash="",
         annotation_version=0,
     ),
     DatasetListing(
         data_object_hash="399146164375493f916025b04d00709c",
-        data_object_path=osp.join(UPDATES_DIR, "no_inference/00023.png"),
+        data_object_path=osp.join(UPDATES_DIR, "no_inference", "00023.png"),
         annotation_hash="818e4e07715f01efd2b8b823163b910d",
         annotation_version=1,
     ),
     DatasetListing(
         data_object_hash="47149106168f7d88fcea9e168608f129",
-        data_object_path=osp.join(UPDATES_DIR, "same_inference/00029.png"),
+        data_object_path=osp.join(UPDATES_DIR, "same_inference", "00029.png"),
         annotation_hash="83c839bd3ca50c68cd17af5395d879d6",
         annotation_version=1,
     ),
     DatasetListing(
         data_object_hash="65383bee429980b89febc3f9b3349379",
-        data_object_path=osp.join(UPDATES_DIR, "no_inference/00010.png"),
+        data_object_path=osp.join(UPDATES_DIR, "no_inference", "00010.png"),
         annotation_hash="",
         annotation_version=0,
     ),
     DatasetListing(
         data_object_hash="66e0373a2a989870fbc2c7791d8e6490",
-        data_object_path=osp.join(UPDATES_DIR, "no_inference/00021.png"),
+        data_object_path=osp.join(UPDATES_DIR, "no_inference", "00021.png"),
         annotation_hash="ea37760e357f44bf15d525022a5a87db",
         annotation_version=1,
     ),
     DatasetListing(
         data_object_hash="a2430513e897d5abcf62a55b8df81355",
-        data_object_path=osp.join(UPDATES_DIR, "diff_inference/00015.png"),
+        data_object_path=osp.join(UPDATES_DIR, "diff_inference", "00015.png"),
         annotation_hash="89723aed8ac59ede5e9884956f8fa90a",
         annotation_version=1,
     ),
     DatasetListing(
         data_object_hash="b5fba326c8247d9e62aa17a109146c02",
-        data_object_path=osp.join(UPDATES_DIR, "same_inference/00040.png"),
+        data_object_path=osp.join(UPDATES_DIR, "same_inference", "00040.png"),
         annotation_hash="131ce3a7527d7d2cbc843092bd7f1bd0",
         annotation_version=1,
     ),
     DatasetListing(
         data_object_hash="def3cbcb30f3254a2a220e51ddf45375",
-        data_object_path=osp.join(UPDATES_DIR, "no_inference/00026.png"),
+        data_object_path=osp.join(UPDATES_DIR, "no_inference", "00026.png"),
         annotation_hash="",
         annotation_version=0,
     ),
     DatasetListing(
         data_object_hash="e299594dc1f79f8e69c6d79a42699822",
-        data_object_path=osp.join(UPDATES_DIR, "diff_inference/00002.png"),
+        data_object_path=osp.join(UPDATES_DIR, "diff_inference", "00002.png"),
         annotation_hash="cc0b2928a6401478688b7a382290c86a",
         annotation_version=1,
     ),
@@ -104,43 +104,71 @@ def test_ls_storage_locations(ldb_instance, workspace_path, data_dir):
     expected = [
         DatasetListing(
             data_object_hash="31ed21a2633c6802e756dd06220b0b82",
-            data_object_path=osp.join(UPDATES_DIR, "no_inference/00028.png"),
+            data_object_path=osp.join(
+                UPDATES_DIR,
+                "no_inference",
+                "00028.png",
+            ),
             annotation_hash="",
             annotation_version=0,
         ),
         DatasetListing(
             data_object_hash="399146164375493f916025b04d00709c",
-            data_object_path=osp.join(UPDATES_DIR, "no_inference/00023.png"),
+            data_object_path=osp.join(
+                UPDATES_DIR,
+                "no_inference",
+                "00023.png",
+            ),
             annotation_hash="818e4e07715f01efd2b8b823163b910d",
             annotation_version=1,
         ),
         DatasetListing(
             data_object_hash="65383bee429980b89febc3f9b3349379",
-            data_object_path=osp.join(UPDATES_DIR, "no_inference/00010.png"),
+            data_object_path=osp.join(
+                UPDATES_DIR,
+                "no_inference",
+                "00010.png",
+            ),
             annotation_hash="",
             annotation_version=0,
         ),
         DatasetListing(
             data_object_hash="66e0373a2a989870fbc2c7791d8e6490",
-            data_object_path=osp.join(UPDATES_DIR, "no_inference/00021.png"),
+            data_object_path=osp.join(
+                UPDATES_DIR,
+                "no_inference",
+                "00021.png",
+            ),
             annotation_hash="ea37760e357f44bf15d525022a5a87db",
             annotation_version=1,
         ),
         DatasetListing(
             data_object_hash="a2430513e897d5abcf62a55b8df81355",
-            data_object_path=osp.join(UPDATES_DIR, "diff_inference/00015.png"),
+            data_object_path=osp.join(
+                UPDATES_DIR,
+                "diff_inference",
+                "00015.png",
+            ),
             annotation_hash="89723aed8ac59ede5e9884956f8fa90a",
             annotation_version=1,
         ),
         DatasetListing(
             data_object_hash="def3cbcb30f3254a2a220e51ddf45375",
-            data_object_path=osp.join(UPDATES_DIR, "no_inference/00026.png"),
+            data_object_path=osp.join(
+                UPDATES_DIR,
+                "no_inference",
+                "00026.png",
+            ),
             annotation_hash="",
             annotation_version=0,
         ),
         DatasetListing(
             data_object_hash="e299594dc1f79f8e69c6d79a42699822",
-            data_object_path=osp.join(UPDATES_DIR, "diff_inference/00002.png"),
+            data_object_path=osp.join(
+                UPDATES_DIR,
+                "diff_inference",
+                "00002.png",
+            ),
             annotation_hash="cc0b2928a6401478688b7a382290c86a",
             annotation_version=1,
         ),
@@ -164,7 +192,8 @@ def test_ls_data_objects(ldb_instance, workspace_path, index_original):
             data_object_hash="1e0759182b328fd22fcdb5e6beb54adf",
             data_object_path=osp.join(
                 ORIGINAL_DIR,
-                "data_objects_only/00014.png",
+                "data_objects_only",
+                "00014.png",
             ),
             annotation_hash="",
             annotation_version=0,
@@ -173,20 +202,31 @@ def test_ls_data_objects(ldb_instance, workspace_path, index_original):
             data_object_hash="3c679fd1b8537dc7da1272a085e388e6",
             data_object_path=osp.join(
                 ORIGINAL_DIR,
-                "data_objects_only/00036.png",
+                "data_objects_only",
+                "00036.png",
             ),
             annotation_hash="",
             annotation_version=0,
         ),
         DatasetListing(
             data_object_hash="982814b9116dce7882dfc31636c3ff7a",
-            data_object_path=osp.join(ORIGINAL_DIR, "has_both/test/00025.png"),
+            data_object_path=osp.join(
+                ORIGINAL_DIR,
+                "has_both",
+                "test",
+                "00025.png",
+            ),
             annotation_hash="818e4e07715f01efd2b8b823163b910d",
             annotation_version=1,
         ),
         DatasetListing(
             data_object_hash="ebbc6c0cebb66738942ee56513f9ee2f",
-            data_object_path=osp.join(ORIGINAL_DIR, "has_both/test/00047.png"),
+            data_object_path=osp.join(
+                ORIGINAL_DIR,
+                "has_both",
+                "test",
+                "00047.png",
+            ),
             annotation_hash="a971780236ad55f28aa6248467f6e27f",
             annotation_version=1,
         ),
@@ -201,7 +241,8 @@ def test_ls_datasets(ldb_instance, ds_a, ds_b):
             data_object_hash="1e0759182b328fd22fcdb5e6beb54adf",
             data_object_path=osp.join(
                 ORIGINAL_DIR,
-                "data_objects_only/00014.png",
+                "data_objects_only",
+                "00014.png",
             ),
             annotation_hash="",
             annotation_version=0,
@@ -210,7 +251,8 @@ def test_ls_datasets(ldb_instance, ds_a, ds_b):
             data_object_hash="2f3533f1e35349602fbfaf0ec9b3ef3f",
             data_object_path=osp.join(
                 ORIGINAL_DIR,
-                "data_objects_only/00017.png",
+                "data_objects_only",
+                "00017.png",
             ),
             annotation_hash="",
             annotation_version=0,
@@ -219,7 +261,8 @@ def test_ls_datasets(ldb_instance, ds_a, ds_b):
             data_object_hash="3c679fd1b8537dc7da1272a085e388e6",
             data_object_path=osp.join(
                 ORIGINAL_DIR,
-                "data_objects_only/00036.png",
+                "data_objects_only",
+                "00036.png",
             ),
             annotation_hash="",
             annotation_version=0,
@@ -228,14 +271,21 @@ def test_ls_datasets(ldb_instance, ds_a, ds_b):
             data_object_hash="95789bb1ac140460cefc97a6e66a9ee8",
             data_object_path=osp.join(
                 ORIGINAL_DIR,
-                "has_both/train/00007.png",
+                "has_both",
+                "train",
+                "00007.png",
             ),
             annotation_hash="a971780236ad55f28aa6248467f6e27f",
             annotation_version=1,
         ),
         DatasetListing(
             data_object_hash="982814b9116dce7882dfc31636c3ff7a",
-            data_object_path=osp.join(ORIGINAL_DIR, "has_both/test/00025.png"),
+            data_object_path=osp.join(
+                ORIGINAL_DIR,
+                "has_both",
+                "test",
+                "00025.png",
+            ),
             annotation_hash="818e4e07715f01efd2b8b823163b910d",
             annotation_version=1,
         ),
@@ -243,14 +293,20 @@ def test_ls_datasets(ldb_instance, ds_a, ds_b):
             data_object_hash="e1c3ef93e4e1cf108fa2a4c9d6e03af2",
             data_object_path=osp.join(
                 ORIGINAL_DIR,
-                "data_objects_only/00011.png",
+                "data_objects_only",
+                "00011.png",
             ),
             annotation_hash="",
             annotation_version=0,
         ),
         DatasetListing(
             data_object_hash="ebbc6c0cebb66738942ee56513f9ee2f",
-            data_object_path=osp.join(ORIGINAL_DIR, "has_both/test/00047.png"),
+            data_object_path=osp.join(
+                ORIGINAL_DIR,
+                "has_both",
+                "test",
+                "00047.png",
+            ),
             annotation_hash="a971780236ad55f28aa6248467f6e27f",
             annotation_version=1,
         ),
@@ -307,7 +363,7 @@ def test_ls_collection_with_workspace_dataset(
     stage_new_workspace(workspace_path)
     os.chdir(workspace_path)
     for dir_path in dirs_to_add:
-        main(["add", f"{os.fspath(dir_path)}"])
+        main(["add", os.fspath(dir_path)])
 
     ws_collection = collection_dir_to_object(
         workspace_path / WorkspacePath.COLLECTION,
