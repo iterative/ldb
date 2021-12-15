967f92be081a752b24b56afc98aa19302ede93f1
Jon Burdo <jon@jonburdo.com>
2021-12-15 10:13:19 -0500

    print error message if python version is unsupported (#106)

--- ldb/main.py
+++ ldb/main.py
@@ -1,6 +1,7 @@
 __all__ = ["main"]
 
 import logging
+import sys
 from logging import Logger
 from types import TracebackType
 from typing import List, Optional, Tuple, Type, Union
@@ -8,6 +9,9 @@ from typing import List, Optional, Tuple, Type, Union
 from ldb.cli import get_main_parser
 
 logger = logging.getLogger(__name__)
+
+MIN_PYTHON_VERSION_INFO = (3, 7)
+MIN_PYTHON_VERSION = ".".join(map(str, MIN_PYTHON_VERSION_INFO))
 
 
 class QuietFormatter(logging.Formatter):
@@ -37,13 +41,17 @@ def main(argv: Optional[List[str]] = None) -> int:
     main_parser = get_main_parser()
     options = main_parser.parse_args(args=argv)
     configure_logger(logger, options.verbose > 0)
-
     try:
-        func = options.func
-    except AttributeError:
-        main_parser.print_usage()
-        return 1
-    try:
+        if sys.version_info < MIN_PYTHON_VERSION_INFO:
+            raise ImportError(
+                f"Python {MIN_PYTHON_VERSION} or greater is required. "
+                f"Unsupported version running:\n{sys.version}",
+            )
+        try:
+            func = options.func
+        except AttributeError:
+            main_parser.print_usage()
+            return 1
         func(options)
     except Exception as exc:  # pylint: disable=broad-except
         logger.exception(exc)
