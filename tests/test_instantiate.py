7cc2d45388348840c577a4ecb780e874b89dc250
Jon Burdo <jon@jonburdo.com>
2022-01-03 17:58:49 -0500

    add inferred export

--- tests/test_instantiate.py
+++ tests/test_instantiate.py
@@ -1,13 +1,23 @@
+import os
 from pathlib import Path
-from typing import Tuple
+from typing import List, Tuple
 
 from ldb.main import main
+from ldb.utils import DATASET_PREFIX, ROOT
 from ldb.workspace import iter_workspace_dir
+
+from .utils import DATA_DIR
 
 
 def get_workspace_counts(workspace_path: Path) -> Tuple[int, int]:
-    all_files = list(iter_workspace_dir(workspace_path))
-    num_annotations = sum(f.name.endswith(".json") for f in all_files)
+    all_files: List[str] = []
+    for entry in iter_workspace_dir(workspace_path):
+        if entry.is_file():
+            all_files.append(entry.name)
+        else:
+            for _, _, files in os.walk(entry.path):
+                all_files.extend(files)
+    num_annotations = sum(f.endswith(".json") for f in all_files)
     num_data_objects = len(all_files) - num_annotations
     return num_data_objects, num_annotations
 
@@ -25,3 +35,11 @@ def test_instantiate_strict(staged_ds_fashion, workspace_path):
 def test_instantiate_annot(staged_ds_fashion, workspace_path):
     main(["instantiate", "-m", "annot"])
     assert get_workspace_counts(workspace_path) == (0, 23)
+
+
+def test_instantiate_infer(workspace_path):
+    main(["stage", "ds:my-dataset"])
+    main(["index", "-m", "infer", os.fspath(DATA_DIR / "inferred/multilevel")])
+    main(["add", f"{DATASET_PREFIX}{ROOT}"])
+    main(["instantiate", "-m", "infer"])
+    assert get_workspace_counts(workspace_path) == (23, 0)
