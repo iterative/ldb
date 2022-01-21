8c7b8febf9e328f5c6ec446e88adb62cd0f9f291
Jon Burdo <jon@jonburdo.com>
2022-01-21 18:03:12 -0500

    fix bug with sort option on windows

--- tests/data.py
+++ tests/data.py
@@ -1,22 +1,22 @@
 from .utils import SORT_DIR
 
 REVERSE_SCRIPT = str(SORT_DIR / "reverse")
-
-BASIC_FILE_QUERY = ["--file", "fs.size > `400`"]
-BASIC_ANNOT_QUERY = ["--query", "@ == `null` || inference.label != `null`"]
-BASIC_QUERIES = [*BASIC_FILE_QUERY, *BASIC_ANNOT_QUERY]
+SORT_Q1 = ["--sort", "python3", f"{REVERSE_SCRIPT}"]
+FILE_Q1 = ["--file", "fs.size > `400`"]
+ANNOT_Q1 = ["--query", "@ == `null` || inference.label != `null`"]
+BASIC_QUERIES = [*FILE_Q1, *ANNOT_Q1]
 # args,data_objs,annots
 QUERY_DATA = [
     ([], 32, 23),
     (["--sample", "1.0"], 32, 23),
     (["--sample", "0.0"], 0, 0),
     (["--limit", "12"], 12, 7),
-    (["--limit", "12", "--sort", f"{REVERSE_SCRIPT}", "--query", "@"], 7, 7),
-    (["--sort", f"{REVERSE_SCRIPT}", "--limit", "12", "--query", "@"], 11, 11),
+    (["--limit", "12", *SORT_Q1, "--query", "@"], 7, 7),
+    ([*SORT_Q1, "--limit", "12", "--query", "@"], 11, 11),
     (["--file", "@"], 32, 23),
     (["--query", "@"], 23, 23),
     (["--file", "fs.size > `400`"], 26, 20),
-    (BASIC_ANNOT_QUERY, 13, 4),
+    (ANNOT_Q1, 13, 4),
     (BASIC_QUERIES, 9, 3),
     (["--limit", "22", *BASIC_QUERIES], 7, 2),
     ([*BASIC_QUERIES, "--file", "fs.size < `600`"], 7, 3),
@@ -33,7 +33,7 @@ QUERY_DATA = [
             '--query=type(inference.label) == `"number"`',
             "--limit=10",
             "--query=inference.label <= label",
-            f"--sort={REVERSE_SCRIPT}",
+            *SORT_Q1,
             "--limit=5",
         ],
         3,
