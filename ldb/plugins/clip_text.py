fbed13bed7c89e3474a48011edc96b483bb1953d
Jon Burdo <jon@jonburdo.com>
2022-02-09 13:26:44 -0500

    add usage message and error handling to pipe plugins (#180)

--- ldb/plugins/clip_text.py
+++ ldb/plugins/clip_text.py
@@ -6,13 +6,22 @@ from .utils import sort_by_iterable
 
 
 def main() -> None:
+    if len(sys.argv) < 2:
+        print(
+            "usage: clip-text <text> [<model_name>]",
+            file=sys.stderr,
+        )
+        sys.exit(1)
     text = sys.argv[1]
     model_name = sys.argv[2] if len(sys.argv) > 2 else None
     data = json.loads(sys.stdin.read())
+    if not data:
+        return
+
     file_paths = [d[1] for d in data]
     similarity = text_similarity(text, file_paths, model_name)
     for data_object_hash, _, _ in sort_by_iterable(data, similarity):
-        print(data_object_hash)
+        print(data_object_hash, flush=True)
 
 
 if __name__ == "__main__":
