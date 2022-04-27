4df0b1a8c0160086a877230e53cd8f32ccf99b47
Jon Burdo <jon@jonburdo.com>
2022-04-27 17:20:41 -0400

    add path option

--- ldb/op_type.py
+++ ldb/op_type.py
@@ -2,6 +2,7 @@ class OpType:
     ANNOTATION_QUERY = "annotation_query"
     JP_ANNOTATION_QUERY = "jp_annotation_query"
     FILE_QUERY = "file_query"
+    PATH_QUERY = "path_query"
     TAG_QUERY = "tag_query"
     NO_TAG_QUERY = "no_tag_query"
     LIMIT = "limit"
