8ab44144e9654dbe58c2a331a72ed9924e9eaeb1
Jon Burdo <jon@jonburdo.com>
2022-04-28 21:22:40 -0400

    throw missing workspace error before applying queries for add and sync

--- ldb/command/sync.py
+++ ldb/command/sync.py
@@ -2,46 +2,19 @@ from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from typing import TYPE_CHECKING, Iterable
 
-from ldb.add import add, delete_missing, process_args_for_add
+from ldb.add import sync
 from ldb.cli_utils import add_data_obj_params
-from ldb.core import get_ldb_instance
-from ldb.dataset import apply_queries
 
 if TYPE_CHECKING:
     from argparse import _SubParsersAction
 
 
-def add_command(options: Namespace) -> None:
-    paths = options.paths
-    query_args = options.query_args
-    if not paths:
-        paths = ["."]
-    ldb_dir = get_ldb_instance()
-    data_object_hashes, annotation_hashes, message = process_args_for_add(
-        ldb_dir,
-        paths,
-    )
-    if message:
-        print(message)
-        print()
-
-    collection = list(
-        apply_queries(
-            ldb_dir,
-            data_object_hashes,
-            annotation_hashes,
-            query_args,
-            warn=False,
-        ),
-    )
-
-    data_object_hashes = {d for d, _ in collection}
-    print("Syncing working dataset...")
-    add(
+def sync_command(options: Namespace) -> None:
+    sync(
         Path("."),
-        collection,
+        options.paths,
+        options.query_args,
     )
-    delete_missing(Path("."), data_object_hashes)
 
 
 def add_parser(
@@ -54,4 +27,4 @@ def add_parser(
         help="Sync workspace to data objects under a certain path",
     )
     add_data_obj_params(parser, dest="query_args")
-    parser.set_defaults(func=add_command)
+    parser.set_defaults(func=sync_command)
