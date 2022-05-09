103de555009cf461a487fb7f534c6c27e321f9f6
Jon Burdo <jon@jonburdo.com>
2022-05-09 15:36:11 -0400

    include init quickstart in top-level stage function

--- ldb/stage.py
+++ ldb/stage.py
@@ -3,6 +3,8 @@ import shutil
 from pathlib import Path
 from typing import Dict, List, Optional, Tuple
 
+from ldb.config import get_ldb_dir
+from ldb.core import init_quickstart
 from ldb.dataset import Dataset, DatasetVersion
 from ldb.exceptions import LDBException
 from ldb.path import InstanceDir, WorkspacePath
@@ -25,6 +27,24 @@ from ldb.workspace import (
 
 
 def stage(
+    dataset_identifier: str,
+    workspace_path: Path,
+    force: bool = False,
+    ldb_dir: Optional[Path] = None,
+) -> None:
+    if ldb_dir is None:
+        ldb_dir = get_ldb_dir()
+    if not ldb_dir.is_dir():
+        ldb_dir = init_quickstart()
+    stage_with_instance(
+        ldb_dir,
+        dataset_identifier,
+        workspace_path,
+        force,
+    )
+
+
+def stage_with_instance(
     ldb_dir: Path,
     dataset_identifier: str,
     workspace_path: Path,
