4efaa270a1e5254949b8ee4602283928449bdbaf
Jon Burdo <jon@jonburdo.com>
2022-07-05 14:31:30 -0400

    feat(ds): add subcommands `list` and `del` to `ldb ds`

--- ldb/command/ds.py
+++ ldb/command/ds.py
@@ -2,18 +2,26 @@ from argparse import ArgumentParser, Namespace
 from typing import TYPE_CHECKING, Iterable
 
 from ldb.core import get_ldb_instance
-from ldb.ds import ds, print_ds_listings
+from ldb.ds import delete_datasets, ds, print_ds_listings
 
 if TYPE_CHECKING:
     from argparse import _SubParsersAction
 
 
-def ds_command(options: Namespace) -> None:  # pylint: disable=unused-argument
+def ds_list_command(
+    options: Namespace,  # pylint: disable=unused-argument
+) -> None:
     ldb_dir = get_ldb_instance()
     ds_listings = ds(
         ldb_dir,
     )
     print_ds_listings(ds_listings)
+
+
+def ds_del_command(
+    options: Namespace,
+) -> None:  # pylint: disable=unused-argument
+    delete_datasets(get_ldb_instance(), options.datasets)
 
 
 def add_parser(
@@ -23,6 +31,28 @@ def add_parser(
     parser = subparsers.add_parser(
         "ds",
         parents=parents,
-        help="List datasets",
+        help="Manage saved datasets",
     )
-    parser.set_defaults(func=ds_command)
+    ds_subparsers = parser.add_subparsers(
+        dest="subcommand",
+        required=True,
+    )
+
+    ds_list_parser = ds_subparsers.add_parser(
+        "list",
+        parents=parents,
+        help="List saved datasets",
+    )
+    ds_list_parser.set_defaults(func=ds_list_command)
+
+    ds_del_parser = ds_subparsers.add_parser(
+        "del",
+        parents=parents,
+        help="Delete datasets",
+    )
+    ds_del_parser.add_argument(
+        dest="datasets",
+        metavar="<dataset>",
+        nargs="+",
+    )
+    ds_del_parser.set_defaults(func=ds_del_command)
--- ldb/ds.py
+++ ldb/ds.py
@@ -1,9 +1,12 @@
+import os
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Iterable, Iterator
 
 from ldb.dataset import iter_datasets
-from ldb.utils import format_dataset_identifier
+from ldb.exceptions import DatasetNotFoundError
+from ldb.path import InstanceDir
+from ldb.utils import format_dataset_identifier, parse_dataset_identifier
 
 
 @dataclass
@@ -25,3 +28,32 @@ def ds(
 def print_ds_listings(ds_listings: Iterable[DSListing]) -> None:
     for item in ds_listings:
         print(format_dataset_identifier(item.name, item.latest_version))
+
+
+def delete_datasets(ldb_dir: Path, ds_identifiers: Iterable[str]) -> None:
+    ds_dir = os.path.join(ldb_dir, InstanceDir.DATASETS)
+    ds_info = []
+    for ds_ident in ds_identifiers:
+        name, version = parse_dataset_identifier(ds_ident)
+        if version is not None:
+            raise ValueError(
+                f"expected dataset identifier without version, got {ds_ident}",
+            )
+        path = os.path.join(ds_dir, name)
+        ds_info.append((ds_ident, path))
+
+    # fail at the beginning if any datasets are missing
+    for ds_ident, path in ds_info:
+        if not os.path.isfile(path):
+            raise DatasetNotFoundError(
+                f"Dataset not found: {ds_ident}",
+            )
+
+    for ds_ident, path in ds_info:
+        try:
+            os.unlink(path)
+        except FileNotFoundError as exc:
+            raise DatasetNotFoundError(
+                f"Dataset not found: {ds_ident}",
+            ) from exc
+        print(f"Deleted {ds_ident}")
