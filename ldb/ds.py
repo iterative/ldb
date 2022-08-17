05ef897bc6cd13072abc7d01d41ce2e2cedc9abc
Jon Burdo <jon@jonburdo.com>
2022-08-17 17:05:45 -0400

    feat(ds): delete collections along with dataset
    When a dataset is deleted with `ldb ds del` the collection objects that its versions reference will also be deleted if no other dataset versions point to them

--- ldb/ds.py
+++ ldb/ds.py
@@ -3,10 +3,20 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Iterable, Iterator
 
-from ldb.dataset import iter_datasets
+from ldb.dataset import (
+    Dataset,
+    DatasetVersion,
+    get_all_dataset_version_identifiers,
+    iter_datasets,
+)
 from ldb.exceptions import DatasetNotFoundError
 from ldb.path import InstanceDir
-from ldb.utils import format_dataset_identifier, parse_dataset_identifier
+from ldb.utils import (
+    format_dataset_identifier,
+    get_hash_path,
+    load_data_file,
+    parse_dataset_identifier,
+)
 
 
 @dataclass
@@ -46,6 +56,8 @@ def print_ds_listings(
 
 def delete_datasets(ldb_dir: Path, ds_identifiers: Iterable[str]) -> None:
     ds_dir = os.path.join(ldb_dir, InstanceDir.DATASETS)
+    ds_version_dir = os.path.join(ldb_dir, InstanceDir.DATASET_VERSIONS)
+    collection_dir = os.path.join(ldb_dir, InstanceDir.COLLECTIONS)
     ds_info = []
     for ds_ident in ds_identifiers:
         name, version = parse_dataset_identifier(ds_ident)
@@ -63,11 +75,36 @@ def delete_datasets(ldb_dir: Path, ds_identifiers: Iterable[str]) -> None:
                 f"Dataset not found: {ds_ident}",
             )
 
+    collection_identifiers = get_all_dataset_version_identifiers(ldb_dir)
     for ds_ident, path in ds_info:
         try:
-            os.unlink(path)
+            dataset = Dataset.parse(load_data_file(Path(path)))
         except FileNotFoundError as exc:
             raise DatasetNotFoundError(
                 f"Dataset not found: {ds_ident}",
             ) from exc
+        for i, ds_version in dataset.numbered_versions().items():
+            version_path = get_hash_path(
+                Path(ds_version_dir),
+                ds_version,
+            )
+            version_obj = DatasetVersion.parse(load_data_file(version_path))
+            ds_version_ident = format_dataset_identifier(dataset.name, i)
+            refs = collection_identifiers[version_obj.collection]
+            refs.remove(ds_version_ident)
+            if not refs:
+                collection_path = str(
+                    get_hash_path(
+                        Path(collection_dir),
+                        version_obj.collection,
+                    ),
+                )
+                os.unlink(collection_path)
+                try:
+                    os.rmdir(
+                        os.path.split(collection_path.rstrip(os.path.sep))[0],
+                    )
+                except OSError:
+                    pass
+        os.unlink(path)
         print(f"Deleted {ds_ident}")
