9b3fbabff7602cf5c0232fa17198292f71bb02db
Jon Burdo <jon@jonburdo.com>
2022-09-29 12:47:35 -0400

    duckdb backend for data_object_meta and annotation

--- ldb/db/abstract.py
+++ ldb/db/abstract.py
@@ -1,14 +1,17 @@
-from ldb.index.utils import AnnotationMeta
-from ldb.index.utils import DataObjectMeta as DataObjectMetaT
-from ldb.objects.annotation import Annotation
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ldb.index.utils import AnnotationMeta
+    from ldb.index.utils import DataObjectMeta as DataObjectMetaT
+    from ldb.objects.annotation import Annotation
 
 
 class AbstractDB:
     def add_pair(
         self,
         data_object_hash: str,
-        data_object_meta: DataObjectMetaT,
-        annotation: Annotation = None,
-        annotation_meta: AnnotationMeta = None,
+        data_object_meta: "DataObjectMetaT",
+        annotation: "Annotation" = None,
+        annotation_meta: "AnnotationMeta" = None,
     ) -> None:
         raise NotImplementedError
